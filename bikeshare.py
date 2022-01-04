a09338ff611b3104f670da6f5135869e9a2ce97d
unlysted <jwestphal@symplr.com>
2022-01-04 18:55:10 -0500

    refactor : added comment to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# This marks the beginning of the branch name "documentation"
+
 CITY_DATA = { 'chicago': 'chicago.csv','new york city': 'new_york_city.csv','washington': 'washington.csv' }
 
 # Added the word 'all' so the month's calendar number equals its index
