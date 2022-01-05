e1a473cbc3062b58be4f5256fe1e80a70d9adab1
unlysted <jwestphal@symplr.com>
2022-01-04 19:00:54 -0500

    refactor : added comment to bikeshare.py for refactoring branch

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# This comment marks the beginning of the branch named "refactoring"
+
 CITY_DATA = { 'chicago': 'chicago.csv','new york city': 'new_york_city.csv','washington': 'washington.csv' }
 
 # Added the word 'all' so the month's calendar number equals its index
